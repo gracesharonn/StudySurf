82b6aa99b92f0e079300d44a5f86af042d50e6ab
Grace Sharonn <125627135+gracesharonn@users.noreply.github.com>
2024-12-27 15:54:32 +0800

    Add files via upload

---
+++ src/calculator/calc-fe/src/constants.ts
@@ -0,0 +1,10 @@
+const SWATCHES = [
+    "#ffffff",  // white
+    "#ee3333",  // red
+    "#228be6",  // blue
+    "#00aa00",  // dark green
+    "#fab005",  // yellow
+    "#fd7e14",  // orange
+];
+  
+export { SWATCHES };
